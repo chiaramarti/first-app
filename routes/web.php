d679f882582b1af71a1a985cb59807ddd3a55b32
chiaramarti <martinellichiara94@gmail.com>
2024-05-21 15:10:49 +0200

    create - fixed

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index'); // Visualizzazione dell'elenco dei prodotti
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show'); // Visualizzazione dei dettagli di un singolo prodotto
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // Creazione di un nuovo prodotto (form)
+
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show'); // Visualizzazione dei dettagli di un singolo prodotto
+
+
 Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // Salvataggio del nuovo prodotto
 Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit'); // Modifica di un prodotto esistente (form)
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update'); // Aggiornamento del prodotto esistente
